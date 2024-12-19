5f6a02ae6621518b54c260aeaabf2d2e888d3279
Spayralbe <stefan@thehyve.nl>
2024-12-19 17:28:56 +0100

    Validate ORM relationships in tests

--- tests/omop_cdm/regular/cdm54/test_cdm54.py
+++ tests/omop_cdm/regular/cdm54/test_cdm54.py
@@ -3,7 +3,7 @@ from sqlalchemy import Engine, inspect
 
 import src.omop_cdm.regular.cdm54 as cdm54
 from src.omop_cdm.constants import CDM_SCHEMA, VOCAB_SCHEMA
-from tests.conftest import create_all_tables, temp_schemas
+from tests.conftest import create_all_tables, temp_schemas, validate_relationships
 from tests.omop_cdm.table_sets import CDM54_NON_VOCAB, VOCAB
 
 SCHEMA_MAP = {VOCAB_SCHEMA: "vocab54", CDM_SCHEMA: "cdm54"}
@@ -22,3 +22,4 @@ def test_create_tables_cdm54_regular(cdm54_regular_engine: Engine):
         cdm_tables = inspect(engine).get_table_names(SCHEMA_MAP[CDM_SCHEMA])
         assert set(vocab_tables) == VOCAB
         assert set(cdm_tables) == CDM54_NON_VOCAB
+        validate_relationships(cdm54_regular_engine, cdm54.Person)
